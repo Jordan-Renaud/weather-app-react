5a57c1a91e39269e0c2acd4a771b0205f9996cb6
Jordan Renaud <jordan.dj.renaud@gmail.com>
2021-04-14 11:06:13 +0100

    add current weather information for current location

--- src/Weather.js
+++ src/Weather.js
@@ -1,59 +1,131 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Weather.css";
 import Forecast from "./Forecast";
 
-export default function Weather() {
-    return <div className="Weather">
-        <div className="todays-weather">
-        <div className="current-weather">
-          <img
-            className="current-weather-img"
-            src="weather_icons/cloudy.png"
-            alt="weather_img"
-          />
-          <p className="current-temp">16</p>
-          <button className="celius-to-farenheit default-button">°C</button>
-          <p className="weather-description">Cloudy</p>
-        </div>
+//conversions -windspeed to compass points
+function windDirectionConversion(degree) {
+    if (degree < 22.5 || degree > 337.5) {
+        return "N";
+    } else if (degree < 67.5) {
+        return "NE";
+    } else if (degree < 112.5) {
+        return "E";
+    } else if (degree < 157.5) {
+        return "SE";
+    } else if (degree < 202.5) {
+        return "S";
+    } else if (degree < 247.5) {
+        return "SW";
+    } else if (degree < 292.5) {
+        return "W";
+    } else if (degree < 337.5) {
+        return "NW";
+    } else {
+        return "Error";
+    };
+};
 
-        <div className="details default-box">
-          <div className="grid-container">
-            <div><p className="low label">L</p></div>
-            <div>
-              <p className="low-temp data">8°</p>
-            </div>
-            <div><p className="high label">H</p></div>
-            <div>
-              <p className="high-temp data">20°</p>
-            </div>
-            <div>
-              <img
-                className="icon"
-                src="weather_icons/wind_flag.png"
-                alt="wind flag"
-              />
-            </div>
-            <div><p className="wind-speed data">11km/h NW</p></div>
-            <div>
-              <img
-                className="icon"
-                src="weather_icons/umbrella.png"
-                alt="umbrella icon"
-              />
-            </div>
-            <div>
-              <p className="rain-percentage data"><span>36</span>%</p>
+//conversions -weathercode to img
+function weatherCodeToImg(code, isDayTime) {
+    if ((code >= 200 && code <= 202) || (code >= 230 && code <= 232)) {
+        return isDayTime ? "weather_icons/rain_lightning.png" : "weather_icons/night_rain_lightning.png";
+    } else if (code >= 210 && code <= 221) {
+        return isDayTime ? "weather_icons/lightning.png" : "weather_icons/night_lightning.png";
+    } else if (code >= 300 && code <= 531) {
+        return isDayTime ? "weather_icons/rain.png" : "weather_icons/night_rain.png";
+    } else if (code >= 600 && code <= 602) {
+        return isDayTime ? "weather_icons/snow.png" : "weather_icons/night_snow.png";
+    } else if (code >= 611 && code <= 622) {
+        return "weather_icons/sleet.png";
+    } else if (code == 800) {
+        return isDayTime ? "weather_icons/clear.png" : "weather_icons/night_clear.png";
+    } else if (code == 801 || code == 802) {
+        return isDayTime ? "weather_icons/scattered_clouds.png" : "weather_icons/night_cloudy.png";
+    } else if (code == 803) {
+        return "weather_icons/cloudy.png";
+    } else if (code == 804) {
+        return "weather_icons/very_cloudy.png";
+    } else {
+        return "error";
+    }
+};
+
+export default function Weather(props) {
+  let [openWeatherJSON, setOpenWeatherJSON] = useState(null);
+  let [accuweatherJSON, setaccuweatherJSON] = useState(null);
+
+  useEffect(() => {
+    if (props.openWeather) {
+      setOpenWeatherJSON(props.openWeather)
+      setaccuweatherJSON(props.accuweather)
+    }
+  }, [props.openWeather]);
+  
+
+  if (openWeatherJSON == null) {
+    return <div></div>
+  } else {
+    const currentTemp = Math.floor(openWeatherJSON.list[0].main.temp);
+    const weatherIcon = openWeatherJSON.list[0].weather[0].id;
+    const weatherDescription = openWeatherJSON.list[0].weather[0].main;
+    const low = Math.floor(accuweatherJSON.DailyForecasts[0].Temperature.Minimum.Value);
+    const high = Math.floor(accuweatherJSON.DailyForecasts[0].Temperature.Maximum.Value);
+    const windSpeed = Math.round(openWeatherJSON.list[0].wind.speed);
+    const windDirection = windDirectionConversion(openWeatherJSON.list[0].wind.deg);
+    const rainChance = openWeatherJSON.list[0].pop;
+
+  return <div className="Weather">
+          <div className="todays-weather">
+          <div className="current-weather">
+            <img
+              className="current-weather-img"
+              src={weatherCodeToImg(weatherIcon, true)}
+              alt="weather_img"
+            />
+            <p className="current-temp">{currentTemp}</p>
+            <button className="celius-to-farenheit default-button">°C</button>
+            <p className="weather-description">{weatherDescription}</p>
+          </div>
+
+          <div className="details default-box">
+            <div className="grid-container">
+              <div><p className="low label">L</p></div>
+              <div>
+                <p className="low-temp data">{low}°</p>
+              </div>
+              <div><p className="high label">H</p></div>
+              <div>
+                <p className="high-temp data">{high}°</p>
+              </div>
+              <div>
+                <img
+                  className="icon"
+                  src="weather_icons/wind_flag.png"
+                  alt="wind flag"
+                />
+              </div>
+              <div><p className="wind-speed data">{windSpeed}km/h {windDirection}</p></div>
+              <div>
+                <img
+                  className="icon"
+                  src="weather_icons/umbrella.png"
+                  alt="umbrella icon"
+                />
+              </div>
+              <div>
+                <p className="rain-percentage data"><span>{rainChance}</span>%</p>
+              </div>
             </div>
           </div>
         </div>
-      </div>
 
-      <div className="future-forecast">
-        <Forecast day="Wed" imgName="weather_icons/clear.png" temp={12} />
-        <Forecast day="Thu" imgName="weather_icons/clear.png" temp={12} />
-        <Forecast day="Fri" imgName="weather_icons/clear.png" temp={12} />
-        <Forecast day="Sat" imgName="weather_icons/clear.png" temp={12} />
-        <Forecast day="Sun" imgName="weather_icons/clear.png" temp={12} />
-      </div>
-      </div>
+        <div className="future-forecast">
+          <Forecast day="Wed" imgName="weather_icons/clear.png" temp={12} />
+          <Forecast day="Thu" imgName="weather_icons/clear.png" temp={12} />
+          <Forecast day="Fri" imgName="weather_icons/clear.png" temp={12} />
+          <Forecast day="Sat" imgName="weather_icons/clear.png" temp={12} />
+          <Forecast day="Sun" imgName="weather_icons/clear.png" temp={12} />
+        </div>
+        </div>
+    } 
 }
--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -1,18 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import "./WeatherApp.css";
 import Weather from "./Weather";
+import axios from "axios";
+
+const openWeatherApiKey = "8d030da1fb95c3588ed90416bc6b659f";
+const accuweatherApiKey = "PeqNwlVzmYXTRnHfn14Juel2uZejZeRS";
 
 export default function WeatherApp() {
+
+  let [openWeatherJSON, setOpenWeatherJSON] = useState(null);
+  let [accuweatherJSON, setAccuweatherJSON] = useState(null);
+  
+   function handleClick(event) {
+     event.preventDefault();
+     navigator.geolocation.getCurrentPosition(handlePosition);
+   }
+
+   function handlePosition(position) {
+     const { latitude, longitude } = position.coords;
+     
+     Promise.all([
+        getOpenWeatherJSON(latitude, longitude),
+        getLocationKey(latitude, longitude)
+    ]).then(handleData);
+   }
+
+   function getOpenWeatherJSON(lat, lon) {
+      const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${openWeatherApiKey}&units=metric`
+      console.log(`open weather map url = ${url}`)
+      return axios.get(url);
+    }
+
+   function getLocationKey(lat, lon) {
+      const url = `https://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=${accuweatherApiKey}&q=${lat}%2C${lon}&details=true`;
+      console.log(`geoposition search url = ${url}`)
+      return axios.get(url).then(getAccuweatherJSON);
+   }
+
+   function getAccuweatherJSON(locationJSON) {
+      const locationKey = locationJSON.data.Key;
+      const url = `https://dataservice.accuweather.com/forecasts/v1/daily/1day/${locationKey}?apikey=${accuweatherApiKey}&metric=true`
+    console.log(`accuweather url = ${url}`)
+    return axios.get(url);
+   }
+
+   function handleData(JSONarray) {
+    setOpenWeatherJSON(JSONarray[0].data);
+    setAccuweatherJSON(JSONarray[1].data);
+   }
+
+   if (openWeatherJSON && accuweatherJSON) {
+     console.log(openWeatherJSON)
+     console.log(accuweatherJSON)
+   }
+
     return <div className="WeatherApp">
         <div className="main-box default-box">
         <header>
           <div>
-            <h1 className="location">London, UK</h1>
+            <h1 className="location">{openWeatherJSON ? `${openWeatherJSON.city.name}, ${openWeatherJSON.city.country}` : null}</h1>
             <h2 className="date">Tuesday 16<sup>th</sup>, March</h2>
           </div>
 
-          <div className="search-location">
-            <button className="get-location-button default-button">
+
+          <div className="search-location Search">
+            <button className="get-location-button default-button" onClick={handleClick}>
               <img src="get_location_icon.png" alt="map icon" />
             </button>
             <input
@@ -20,10 +72,11 @@ export default function WeatherApp() {
               type="text"
               placeholder="&#xF002; Search city"
             />
-          </div>
+            </div>
         </header>
-
-      <Weather />
+        
+        <Weather openWeather={openWeatherJSON} accuweather={accuweatherJSON}/>
+      
       </div>
     </div>
 }
